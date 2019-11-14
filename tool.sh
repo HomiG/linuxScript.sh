eb59b4d9377421aba49b964d41ac490cf361b27b
terminalforlife <terminalforlife@yahoo.com>
2019-11-14 19:23:21 +0000

    Fix up, optimize, make robust, tidy, etc
    A _lot_ has been done, but it's certainly not completely yet.
    
    This is a basically a massive overhaul, as the script was an absolute
    mess beforehand, if I'm brutally honest.
    
    I'm fixing up the broken indenting (using tabs; can change this if
    desired) and ensuring they are consistent, as any language should be,
    in my view.
    
    A _lot_ of awk optimizations have been made, including writing up a new
    awk script which is available on the master branch, as `parsedb.awk`
    and will be called upon within the `tool.sh` script.
    
    Argument processing is far cleaner now, using a standard `while`, -
    `case`, `shift` situation. Also reworking the logic to better fit, be
    more concise, and be easier to edit.
    
    Since it's not the shortest script ever, I've taken to a more condensed
    writing style, to avoid an excessive line count.
    
    That being said, a lot of the comments amended to the lines were moved
    to a more traditional place of above the related code; this allows for
    easier reading, at the expense of more lines through which to scroll.
    
    These are probably just some of the changes.

--- tool.sh
+++ tool.sh
@@ -1,183 +1,225 @@
-#!/bin/bash 
+#!/bin/bash
 
+PARSEDB='parsedb.awk'
 
-#First case "desides" what part of code will be executed according the number of arguments that have been parsed.
-#⬇⬇⬇
-case "$#" in
-"0")
-		echo 1054429-1054406
-		;;
-"2")
-		if [ $1 = "-f" ]
-			then
-			awk -F "|" '/#/ {next} {print}' $2	#skips the lines that contain # 
-		fi
-	;;
-"3")
-		offset=1
-		while [ $# -gt 0 ] 
-		do
-		   case $1 in
-			  -f)
-			  	offset=$[offset+1]
-				filename=${!offset}	#filename variable gets the content of the #offset (e.g. $3) argument. 
-			  	offset=$[offset-1]
-				;;
-			  --firstnames)
-			  		columne_mode='$3'	# $3 is the collumn that contains firstnames	
-				;;
-			  --lastnames)
-			   		columne_mode='$2'	# $2 is the collumn that contains lastnames
-				;;
-			  --socialmedia)
-					columne_mode='$9'	# $9 is the collumn that contains social media
-				;;	
-				
-		   esac
-			shift
-		done
-		
-		if [[ "$columne_mode" == '$2' || "$columne_mode" == '$3' ]]
-		then
-			awk -F "|" '{print '"$columne_mode"' | "sort" }' $filename | uniq
-		fi
-		
-		if [ "$columne_mode" == '$9' ]
-		then
-			awk -F "|" '{print $9 | "sort" }' dates.dat | uniq -c | tr -dc '[:print:]\n'| awk '{print $2 " " $1}'	#First awk prints the socialMediaUsed Sorted, then the uniq -c counts how many times each
-																													#socialMedia appears in the list. (The tr -dc... prevent column text overwriting on "Terminal 
-																													#printing" by striping the special characters.) Finally the last awk prints the data at the right
-																													#order. P.S. The last 2 pipelines are being used in order print social media and times used
-																													#in the right order which is --> SOCIAL MEDIA # <-- and not # SOCIAL MEDIA...
-		fi
-	;;
-"4") 
-		offset=2	#variable used to determine desired argument's <> position.
-		bornMode=0 #determins what awk will be executed.
-		
-		while [ $# -gt 0 ] 
-		do
-		   case $1 in
-			  -f)
-				filename=${!offset}	#filename variable gets the content of the #offset argument. 
-				;;
-			  -id)
-				id=${!offset}
-				;;
-				
-			  --born-until)
-			  	date=${!offset}
-				date=$(echo -n $date | sed 's/-//g') #Delete the - from the date parased in the arguments, so the date will be treated as an int e.g. 1980-21-30 will be 19802130
-			  	bornMode=1
-				;;
-			  --born-since)
-			 	date=${!offset}
-				#echo born position $position and $# and $date
-				date=$(echo -n $date | sed 's/-//g') #Delete the - from the date parased in the arguments, so the date will be treated as an int
-			  	bornMode=2
-			  	;;
-				
+FAIL(){
+	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
+	[ $1 -eq 1 ] && exit 1
+}
 
-		   esac
-			shift
-		done
-		
-		case $bornMode in
-		0) # Runs Search with ID
-			awk -F "|" -v pat="$id" '$1 ~ "^"pat"$" {print $2 " " $3 " " $5}' $filename	#-v declears a variable that will be used in awk. Also --> $1 ~ "^"pat"$" <-- means that 
-		;;																	#the search is ONLY on the ID field ($1) searching for the EXACT patern (^ &)
-		1) # Runs born until
-		
-			awk -F "|" '/#/ {next} {print $1 " " $5}' $filename | sed 's/-//g' | awk '$2<='"$date"'{print $1}' > ids.tmp # Creates a "temporary" file that contains the IDS that match the condition --born-until
-			grep -w -f ids.tmp $filename	#Finds and prints the lines from the input file which ids are the same with the ids.temp, -w for identical strings
-			rm ids.tmp	#remove the temporary file.
-			# The temp file is created so the dates will have the "right" format Y-m-d, otherwise the Y m d would not be seperated with - , according to this implemetation.
-		;;
-		2) # Runs born since
-		
-			awk -F "|" '/#/ {next} {print $1 " " $5}' $filename | sed 's/-//g' | awk '$2>='"$date"'{print $1}' > ids.tmp # Creates a "temporary" file that contains the IDS that match the condition --born-since
-			grep -w -f ids.tmp $filename	#Finds and prints the lines from the input file which ids are the same with the ids.temp
-			rm ids.tmp	#remove the temporary file.
-		;;
-		esac
-	;;
+SHOWALL="false"
 
-"6") 
-		offset=2
-		while [ $# -gt 0 ] 
-		do
-		   case $1 in
-			  -f)
-				filename=${!offset}	#filename variable gets the content of the #offset argument. 
-				;;
-			  --born-since)
-				dateA=${!offset}
-				dateA=$(echo -n $dateA | sed 's/-//g') #Delete the - from the date parased in the arguments, so the date will be treated as an int e.g. 1980-21-30 will be 19802130
-				mode=0
-			  	;;
-			  --born-until)
-				dateB=${!offset}
-				dateB=$(echo -n $dateB | sed 's/-//g') #Delete the - from the date parased in the arguments, so the date will be treated as an int e.g. 1980-21-30 will be 19802130			
-				;;
-			  --edit)
-			  	id=${!offset}
-				offset=$((offset+1))
-				column=${!offset}
-				offset=$((offset+1))
-				value=${!offset}
-				offset=$((offset-2))
-				mode=1
-				;;
-			esac
-				shift
-		done
-				
-		case $mode in
-		0)
-			awk -F "|" '/#/ {next} {print $1 " " $5}' $filename | sed 's/-//g' | awk '$2>='"$dateA"' && $2<='"$dateB"'{print $1}' > ids.tmp
-			grep -w -f ids.tmp $filename	#Finds and prints the lines from the input file which ids are the same with the ids.temp
-			rm ids.tmp	#remove the temporary file.
-		;;
-		1)
-		if [ $column -gt 1 ] && [ $column -lt 9 ]
-		then
-			awk  -F "|" -v pat="$id" 'BEGIN{OFS="|"} $1 ~ "^"pat"$"{$'"$column"' = "'"$value"'"}1' $filename #Search for the ID, set output Field Seperator | , replace the value at the column given in the arguments
-																											 #with the new value.
+while [ "$1" ]; do
+	case $1 in
+		-h|--help|-\?)
+			printf "SYNTAX: ${0##*/} [OPTS] FILE\n"
+			exit 0 ;;
+		-va|--show-all)
+			SHOWALL="true" ;;
+		-f|--first-names)
+			COLNUM=3 ;;
+		-l|--last-names)
+			COLNUM=2 ;;
+		-s|--social-media)
+			COLNUM=9 ;;
+		-b|--browser)
+			COLNUM=8 ;;
+		-E)
+			# Not sure what the point in this is:
+			printf "1054429-1054406\n"
+			exit 0 ;;
+		-*)
+			FAIL 1 "$LINENO" "Incorrect argument(s) specified." ;;
+		*)
+			break ;;
+	esac
+	shift
+done
 
-#------> to save the change we made we can create a new file  with the "changes" like:
-# awk  -F "|" -v pat="$id" 'BEGIN{OFS="|"} $1 ~ "^"pat"$"{$'"$column"' = "'"$value"'"}1' > newFile.tmp
-			
-		fi
-		
-		
-		
-		;;
-		esac
-	;;
-	
-esac
+declare -i DEPCOUNT=0
+for DEP in awk uname; {
+	if ! type -fP "$DEP" > /dev/null 2>&1; then
+		FAIL 0 "$LINENO" "Dependency '$DEP' not met."
+		DEPCOUNT+=1
+	fi
+}
 
+[ $DEPCOUNT -eq 0 ] || exit 1
 
+if ! { [ -f "$*" ] && [ -r "$*" ]; }; then
+	FAIL 1 $LINENO "Input file missing or inaccessible."
+else
+	FILENAME=$*
+fi
 
+if [ "$SHOWALL" == 'true' ]; then
+	# Skips the lines beginning with '#' (comment).
+	awk -F "|" '!/^#/' "$1"
+	exit $?
+elif [ -n $COLNUM ]; then
+	if [ -f "$PARSEDB" ] && [ -r "$PARSEDB" ]; then
+		# Must be executed in the same directory as the files.
+		awk -v T="$(uname -s)" -v C=$COLNUM -f "$PARSEDB" "$FILENAME"
+	else
+		FAIL 1 $LINENO "File '$PARSEDB' missing or inaccessible."
+	fi
+fi
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#----------------------------------------------------------------------------------
+#	3)
+#		shift
+#		case $1 in
+#			-f)
+#				FILENAME=$1
+#				let --offset ;;
+#		esac
+#	4)
+#		# Variable used to determine desired argument's <> position.
+#		offset=2
+#
+#		# Determines which awk will be executed.
+#		bornMode=0
+#
+#		while [ $# -gt 0 ]
+#		do
+#			case $1 in
+#				-f)
+#					# Filename variable gets the content of the
+#					# offset argument.
+#					FILENAME=${!offset} ;;
+#				-id)
+#					id=${!offset} ;;
+#				--born-until)
+#					date=${!offset}
+#					printf -v date "%d" "${date//-}"
+#					bornMode=1 ;;
+#				--born-since)
+#					date=${!offset}
+#					# Echo born $position and $# and $date.
+#
+#					# Ensure integer, by globally removing '-'.
+#					printf -v date "%d" "${date//-}"
+#					bornMode=2 ;;
+#			esac
+#			shift
+#		done
+#
+#		case $bornMode in
+#		0) # Runs Search with ID
+#			# The -v flag declares a variable to be used internally.
+#			# Also --> $1 ~ "^"pat"$" <-- means that.
+#			awk -F "|" -v pat="$id" '
+#				$1 ~ "^"pat"$"{print $2 " " $3 " " $5}
+#			' $FILENAME ;;
+#
+#		# The search is ONLY on the ID field ($1) searching for the EXACT
+#		# pattern (^ &).
+#		1) # Runs born until
+#			# Creates a temporary file containing IDs matching the
+#			# --born-until condition.
+#			awk -F "|" '/#/ {next} {print $1 " " $5}' $FILENAME\
+#				| tr -d '-'\
+#				| awk '$2<='"$date"'{print $1}' > ids.tmp
+#
+#			# Finds and prints the lines from the input file whose id's
+#			# are the same with the ids.temp, -w for identical strings.
+#			grep -w -f ids.tmp $FILENAME
+#
+#			# Remove the temporary file.
+#			rm ids.tmp ;;
+#
+#			# The temp file is created so the dates will have the
+#			# "right" format Y-m-d, otherwise the Y m d would not be
+#			# separated with `-`, according to this implementation.
+#		2) # Runs born since
+#			# Creates a "temporary" file containing IDs matching the
+#			# --born-since condition.
+#			awk -F "|" '/#/ {next} {print $1 " " $5}' $FILENAME\
+#				| tr -d '-'\
+#				| awk '$2>='"$date"'{print $1}' > ids.tmp
+#
+#			# Finds and prints the lines from the input file whose IDs
+#			# are the same with the `ids.temp`.
+#			grep -w -f ids.tmp $FILENAME
+#
+#			# Remove the temporary file.
+#			rm ids.tmp ;;
+#		esac ;;
+#	6)
+#		offset=2
+#		while [ $# -gt 0 ]
+#		do
+#			case $1 in
+#				-f)
+#					# Filename variable gets the content of the
+#					# #offset argument.
+#					FILENAME=${!offset} ;;
+#				--born-since)
+#					dateA=${!offset}
+#
+#					# Delete the `-` from the date parsed in
+#					# the arguments, so the date will be
+#					# treated as an int e.g. '1980-21-30' will
+#					# be '19802130'.
+#					printf -v dateA "%d" "${dateA//-}"
+#
+#					mode=0 ;;
+#				--born-until)
+#					dateB=${!offset}
+#
+#					# Delete the `-` from the date parsed in
+#					# the arguments, so the date will be
+#					# treated as an int e.g. '1980-21-30' will
+#					# be '19802130'.
+#					printf -v dateB "%d" "${dateB//-}" ;;
+#				--edit)
+#					id=${!offset}
+#					offset=$((offset+1))
+#					column=${!offset}
+#					offset=$((offset+1))
+#					value=${!offset}
+#					offset=$((offset-2))
+#					mode=1 ;;
+#				esac
+#				shift
+#		done
+#
+#		case $mode in
+#			0)
+#				awk -F "|" '/#/ {next} {print $1 " " $5}' $FILENAME\
+#					| sed 's/-//g'\
+#					| awk '$2>='"$dateA"' && $2<='"$dateB"'{print $1}' > ids.tmp
+#
+#				# Finds and prints the lines from the input file
+#				# containing IDs the same as in the `ids.temp`.
+#				grep -w -f ids.tmp $FILENAME
+#
+#				# Remove the temporary file.
+#				rm ids.tmp ;;
+#			1)
+#				if [ $column -gt 1 ] && [ $column -lt 9 ]
+#				then
+#					# Search for the ID, set Output Field
+#					# Seperator to `|`, then replace the value
+#					# at the column given in the arguments.
+#					awk  -F "|" -v pat="$id" '
+#						BEGIN{OFS="|"}
+#						$1 ~ "^"pat"$"
+#						{
+#							$'"$column"' = "'"$value"'"
+#						}1
+#					' "$FILENAME"
+#
+#				# To save the change we made we can create a new
+#				# file with the "changes", like:
+#				#
+#				#   awk  -F "|" -v pat="$id" '
+#				#   	BEGIN{OFS="|"}
+#				#   	$1 ~ "^"pat"$"
+#				#   	{
+#				#   		$'"$column"' = "'"$value"'"
+#				#   	}1
+#				#   ' > newFile.tmp
+#				fi ;;
+#		esac ;;
+#esac
